5d7dacca3750697cdad29cac7987bc55ade3c98b
omfgitsjack <jackinyiu@gmail.com>
2015-06-10 22:53:57 -0400

    Fixed polling issues

--- public/app/admin-app.js
+++ public/app/admin-app.js
@@ -11,58 +11,62 @@
         var now = DateTimeService.now();
 
         $scope.yesterdayDay = DateTimeService.now().startOf('day');
-        $scope.todayDay = DateTimeService.now().add(1,'days').startOf('day');
+        $scope.todayDay = DateTimeService.now().add(1, 'days').startOf('day');
 
         // If it's today before 4pm, show yesterday's order.
-        $scope.yesterday = function() {
-		$scope.currentDay = $scope.yesterdayDay;
+        $scope.yesterday = function () {
+            $scope.currentDay = $scope.yesterdayDay;
             $scope.currentAggregateOrder = $scope.yesterdayAggregateOrder;
             $scope.currentOrderList = $scope.yesterdayOrderList;
+
         };
 
-        $scope.today = function() {
+        $scope.today = function () {
             $scope.currentDay = $scope.todayDay;
             $scope.currentAggregateOrder = $scope.todayAggregateOrder;
             $scope.currentOrderList = $scope.todayOrderList;
         };
 
-        me.init = function() {
+        me.init = function () {
 
-            me.pollOrders = function() {
-                OrderService.getToday()
-                    .success(function(res) {
+            me.getOrders = function () {
+                return OrderService.getToday()
+                    .success(function (res) {
                         $scope.todayAggregateOrder = $scope.orderService.aggregateTransformer(res);
-                        $scope.todayOrderList = $scope.orderService.orderListTransformer(res)
-                        me.chooseActiveDay();
-                    });
-
-                OrderService.getYesterday()
+                        $scope.todayOrderList = $scope.orderService.orderListTransformer(res);
+                        return true;
+                    })
                     .success(function(res) {
-                        $scope.yesterdayAggregateOrder = $scope.orderService.aggregateTransformer(res);
-                        $scope.yesterdayOrderList = $scope.orderService.orderListTransformer(res);
-                        me.chooseActiveDay();
+                        OrderService.getYesterday()
+                            .success(function (res) {
+                                $scope.yesterdayAggregateOrder = $scope.orderService.aggregateTransformer(res);
+                                $scope.yesterdayOrderList = $scope.orderService.orderListTransformer(res);
+                            });
                     });
             };
 
-            me.chooseActiveDay = function() {
-
+            me.chooseActiveDay = function () {
                 if (0 <= now.get('hour') && now.get('hour') < 16 && now.get('date') === $scope.todayDay.get('date')) {
                     $scope.yesterday();
+                    $scope.prevDayEnabled = true;
                 } else {
+                    $scope.prevDayEnabled = false;
                     $scope.today();
                 }
             };
 
-            me.pollOrders();
+            me.initPoll = function () {
+                $timeout(function () {
+                    me.getOrders().success(function() {
+                        me.initPoll();
+                    });
+                }, 5000);
+            };
 
-            me.initPoll = function() {
-		$timeout(function() {
-                	me.pollOrders();
-			me.initPoll();
-            	}, 5000);
-	    };
-
-	    me.initPoll();
+            me.getOrders().success(function() {
+                me.chooseActiveDay();
+            });
+            me.initPoll();
         };
 
         me.init();
