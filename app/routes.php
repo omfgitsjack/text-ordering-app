9c3890240b52ac37cc4a891af7280463e5fd59c3
omfgitsjack <jackinyiu@gmail.com>
2015-07-07 20:44:20 -0400

    Updated logic to match OK

--- app/routes.php
+++ app/routes.php
@@ -28,22 +28,39 @@ Route::post('api/authenticate', 'AuthenticateController@sendAuthToken');
 Route::get('api/orders/current', array('after' => 'allowOrigin', 'uses' => 'OrdersController@currentOrders'));
 
 Route::post('api/twilio', function() {
+	// RESPONSE CONSTANTS
+	const CONFIRM_ORDER = "ok";
+	const CANCEL_ORDER = "cancel";
+
 	$auth = Authentication::where('phone', Input::get('From'))->orderBy('id', 'desc')->first();
 	$twiml = new Services_Twilio_Twiml();
 	$WECHATACC = 'ucafe_ca';
-	
-	if (!$auth->verified && $auth->created_at->diffInSeconds(Carbon::now(new DateTimeZone('America/Toronto'))) < 60 * 5) {
-		$auth->verified = true;
+
+	var responseString = strtolower(Input::get('Body'));
+
+	if (responseString == CONFIRM_ORDER) {
+		if (!$auth->verified && $auth->created_at->diffInSeconds(Carbon::now(new DateTimeZone('America/Toronto'))) < 60 * 5) {
+			// Save and confirm
+			$auth->verified = true;
+			$auth->save();
+
+			$msg = '订单已确认, 您的午饭会在1点到达 :) 如果想联络我们的话，请发短信给我们的微信：' . $WECHATACC;
+		} else {
+			// Reject
+			$msg = "不好意思， 您没有在规定时间内回复'OK'. 您的订单没有成功录入我们的系统， 请回到ucafe.ca从新订单并收到短信后立即回复”ok” ：)";
+		}
+	} else if (responseString == CANCEL_ORDER) {
+		$auth->verified = false;
 		$auth->save();
 
-		$twiml->message('订单已确认, 您的午饭会在1点到达 :) 如果想联络我们的话，请发短信给我们的微信：' . $WECHATACC);
-		$response = Response::make($twiml, 200);
-		$response->header('Content-Type', 'text/xml');	
+		$msg = "我们取消了你的订单";
 	} else {
-		$twiml->message("不好意思， 您没有立即确认”ok”, 您的订单没有成功录入我们的系统， 请回到ucafe.ca从新订单并收到短信后立即回复”ok” ：)");
-		$response = Response::make($twiml, 200);
-		$response->header('Content-Type', 'text/xml');
+		$msg = '如果想联络我们的话，请发短信给我们的微信：' . $WECHATACC;
 	}
+
+	$twiml->message($msg);
+	$response = Response::make($twiml, 200);
+	$response->header('Content-Type', 'text/xml');	
 
 	return $response;
 });
