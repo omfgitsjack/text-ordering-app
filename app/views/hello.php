def86f7f5a084a29ace3a83c711fabd7b62f7cbb
Jack Yiu <jackinyiu@gmail.com>
2015-09-08 09:14:31 -0400

    removed bootstrap dependencies

--- app/views/hello.php
+++ app/views/hello.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>UCafe - Delivery chinese food in University of Toronto Scarborough</title>
+	<title>UFan - Delivery chinese food in University of Toronto Scarborough</title>
 	<link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon"/>
 	<link rel="stylesheet" href="https://bootswatch.com/yeti/bootstrap.min.css">
 	<link href="build/vendor.css" rel="stylesheet">
@@ -20,5 +20,4 @@
 
 <script src="build/vendor.js"></script>
 <script src="build/app.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </html>
