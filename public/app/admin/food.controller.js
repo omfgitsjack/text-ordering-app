b68d6f7326b474c0bd49bdb99d864d4c30aeaea7
Jack Yiu <jackinyiu@gmail.com>
2015-09-10 10:35:13 -0400

    Added ability to modify price & post tax price

--- public/app/admin/food.controller.js
+++ public/app/admin/food.controller.js
@@ -7,12 +7,12 @@
 
     function foodController($scope, FoodService, $mdDialog) {
 
-        var getFoodMenu = function() {
+        var getFoodMenu = function () {
             FoodService.getAll()
                 .success(function (res) {
                     $scope.food = res;
 
-                    $scope.$watch('food', function(newVal, oldVal) {
+                    $scope.$watch('food', function (newVal, oldVal) {
                         if (newVal !== oldVal) {
                             FoodService
                                 .update(newVal);
@@ -24,49 +24,51 @@
 
         getFoodMenu();
 
-        $scope.edit = function(ev, item) {
+        $scope.edit = function (ev, item) {
             // Save ref to current item
             $scope.curItem = item;
 
             $mdDialog.show({
-              controller: DialogController,
-              templateUrl: 'app/admin/editFood.tmpl.html',
-              parent: angular.element(document.body),
-              locals: {
-                item: angular.copy(item)
-              },
-              targetEvent: ev,
+                controller: DialogController,
+                templateUrl: 'app/admin/editFood.tmpl.html',
+                parent: angular.element(document.body),
+                locals: {
+                    item: angular.copy(item)
+                },
+                targetEvent: ev,
             })
-            .then(function(newItem) {
-                // Hardcoding the way we update the new item.
-                $scope.curItem.name = newItem.name;
-                $scope.curItem.description = newItem.description;
-                $scope.curItem.ingredients = newItem.ingredients;
-                $scope.curItem.spicy = newItem.spicy;
-                $scope.curItem = null;
-                console.log('You said the information was "' + newItem + '".');
-            }, function() {
-                $scope.curItem = null;
-                $scope.alert = 'You cancelled the dialog.';
-            });
+                .then(function (newItem) {
+                    // Hardcoding the way we update the new item.
+                    $scope.curItem.name = newItem.name;
+                    $scope.curItem.description = newItem.description;
+                    $scope.curItem.ingredients = newItem.ingredients;
+                    $scope.curItem.spicy = newItem.spicy;
+                    $scope.curItem.price = newItem.price;
+                    $scope.curItem.taxedprice = newItem.taxedprice;
+                    $scope.curItem = null;
+                    console.log('You said the information was "' + newItem + '".');
+                }, function () {
+                    $scope.curItem = null;
+                    $scope.alert = 'You cancelled the dialog.';
+                });
 
             function DialogController($scope, $mdDialog, item) {
                 $scope.item = item;
                 console.log(item);
-                $scope.closeDialog = function(item) {
+                $scope.closeDialog = function (item) {
                     $mdDialog.hide(item);
-                }
-                $scope.cancelDialog = function() {
+                };
+                $scope.cancelDialog = function () {
                     $mdDialog.cancel('canceled');
-                }
+                };
             }
 
             function _update(srcObj, destObj) {
-              for (var key in destObj) {
-                if(destObj.hasOwnProperty(key) && srcObj.hasOwnProperty(key)) {
-                  destObj[key] = srcObj[key];
+                for (var key in destObj) {
+                    if (destObj.hasOwnProperty(key) && srcObj.hasOwnProperty(key)) {
+                        destObj[key] = srcObj[key];
+                    }
                 }
-              }
             }
         }
 
