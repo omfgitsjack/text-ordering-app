e79bef475ccb4c9af94a63f7dab7760d25bc53ff
Jack Yiu <jackinyiu@gmail.com>
2015-09-26 23:31:29 -0400

    Modified footer more

--- public/app/app.controller.js
+++ public/app/app.controller.js
@@ -8,6 +8,7 @@
     function appController($scope, $mdDialog) {
 
         $scope.showQRCode = showQRCode;
+        $scope.showAboutUs = showAboutUs;
 
         function showQRCode() {
             $mdDialog.show({
@@ -17,6 +18,19 @@
                     }
                 },
                 templateUrl: 'app/wechatqr.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose: true
+            });
+        }
+
+        function showAboutUs() {
+            $mdDialog.show({
+                controller: function($mdDialog) {
+                    $scope.close = function() {
+                        $mdDialog.hide();
+                    }
+                },
+                templateUrl: 'app/aboutus.html',
                 parent: angular.element(document.body),
                 clickOutsideToClose: true
             });
